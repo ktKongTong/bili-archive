79a80cb15050d364fa89435fdc1a3bcbfb1f0d68
ktKongTong <kt@ktlab.io>
2025-03-20 17:06:30 +0800

    feat: add ESLint configuration and update Next.js config to ignore builds
    Signed-off-by: ktKongTong <kt@ktlab.io>

--- fuma/next.config.mjs
+++ fuma/next.config.mjs
@@ -3,6 +3,7 @@ import { withContentCollections } from "@content-collections/next";
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  ignoreDuringBuilds: true,
 };
 
 export default withContentCollections(config);
